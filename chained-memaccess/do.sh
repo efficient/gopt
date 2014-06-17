ca9ad1d8f7eaaea181fe155280520755cd781c7f
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-06-17 16:30:16 -0400

    Delete old stuff

--- chained-memaccess/do.sh
+++
@@ -1,8 +0,0 @@
-gnuplot plot-chained-memaccess.pg
-ps2pdf chained-memaccess.ps
-pdfcrop --margins 10 chained-memaccess.pdf 
-mv chained-memaccess-crop.pdf chained-memaccess.pdf
-if [ $# -eq 0 ]
-  then
-	open chained-memaccess.pdf &
-fi
