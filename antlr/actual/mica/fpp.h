b7c7cd47621ba0cabd5edc45653686c3e123c16a
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-07-07 18:27:12 -0400

    Start MICA microbenchmark

---
+++ antlr/actual/mica/fpp.h
@@ -0,0 +1,11 @@
+#define PREFETCH(x)	{}					// Just a hint
+#define FPP_SET(n, i) (n | (1 << i))	// Set the ith bit of n
+	
+// Prefetch, Save, and Switch
+#define FPP_PSS(addr, label) \
+do {\
+	__builtin_prefetch(addr); \
+	batch_rips[I] = &&label; \
+	I = (I + 1) & BATCH_SIZE_;	\
+	goto *batch_rips[I]; \
+} while(0)
---
+++ antlr/actual/mica/param.h
@@ -0,0 +1,12 @@
+#define SLOTS_PER_BKT 8
+
+#define BATCH_SIZE 8
+#define BATCH_SIZE_ 7
+
+#define NUM_PKTS (16 * 1024 * 1024)
+
+#define HT_INDEX_SID 1
+#define HT_INDEX_N (8 * 1024 * 1024)		// Number of hash index buckets (size = x 64)
+
+#define HT_LOG_SID 2
+#define HT_LOG_CAP (16 * 1024 * 1024)		// Number of key-value items in log (size = x 16)
