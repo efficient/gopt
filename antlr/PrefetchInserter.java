1d3860e8d8b5aa645b8ed1c81dd8632f7275aa85
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-07-15 17:53:22 -0400

    Fix ANTLR code to use label names and macros

--- antlr/PrefetchInserter.java
+++ antlr/PrefetchInserter.java
@@ -17,15 +17,15 @@ public class PrefetchInserter extends CBaseListener {
 
 	@Override
 	public void enterPostfixExpression(CParser.PostfixExpressionContext ctx) {
-		if(ctx.getText().startsWith("PREFETCH")) {
+		if(ctx.getText().startsWith("FPP_EXPENSIVE")) {
 			int start = ctx.start.getTokenIndex();
 			int stop = ctx.stop.getTokenIndex();
-			if(stop == start) {		// Only "PREFETCH"
+			if(stop == start) {		// Only "FPP_EXPENSIVE"
 				return;
 			}
 			
 			if(ctx.getChildCount() != 4) {
-				System.err.println("ERROR: Wrong use of PREFETCH(1). Aborting");
+				System.err.println("ERROR: Wrong use of FPP_EXPENSIVE(1). Aborting");
 				System.exit(-1);
 			}
 			
@@ -33,15 +33,15 @@ public class PrefetchInserter extends CBaseListener {
 			String ch3 = ctx.getChild(3).getText();
 			
 			if(!(ch1.contentEquals("(") && ch3.contentEquals(")"))) {
-				System.err.println("ERROR: Wrong use of PREFETCH (2). Aborting");
+				System.err.println("ERROR: Wrong use of FPP_EXPENSIVE(2). Aborting");
 				System.exit(-1);
 			}
 			
-			debug.println("Found PREFETCH. Inserting PSS and label.");
+			debug.println("Found FPP_EXPENSIVE. Inserting PSS and fpp_label.");
 			rewriter.replace(start, "FPP_PSS");
-			rewriter.insertBefore(stop, ", label_" + nextLabel);
+			rewriter.insertBefore(stop, ", fpp_label_" + nextLabel);
 			
-			// Find the ";" after the PREFETCH statement. Valid AST ensures that
+			// Find the ";" after the FPP_EXPENSIVE statement. Valid AST ensures that
 			// there is one
 			int semicolonIndex = stop;
 			for(; semicolonIndex < tokens.size(); semicolonIndex ++) {
@@ -50,7 +50,7 @@ public class PrefetchInserter extends CBaseListener {
 				}
 			}
 			
-			rewriter.insertAfter(semicolonIndex, "\nlabel_" + nextLabel + ":\n");
+			rewriter.insertAfter(semicolonIndex, "\nfpp_label_" + nextLabel + ":\n");
 			nextLabel ++;
 		}
 	}
--- antlr/test/mica/nogoto.c
+++ antlr/test/mica/nogoto.c
@@ -6,7 +6,7 @@ void process_pkts_in_batch(LL *pkt_lo)
         int key_tag = HASH_TO_TAG(key_hash);
         int ht_bucket = HASH_TO_BUCKET(key_hash);
         
-        PREFETCH(&ht_index[ht_bucket]);
+        FPP_EXPENSIVE(&ht_index[ht_bucket]);
         LL *slots = ht_index[ht_bucket].slots;
         
         int i, found = 0;
@@ -14,9 +14,10 @@ void process_pkts_in_batch(LL *pkt_lo)
         for(i = 0; i < SLOTS_PER_BKT; i ++) {
             
             // Tag matched
-            if(SLOT_TO_TAG(slots[i]) == key_tag) {
+            if(SLOT_TO_TAG(slots[i]) == key_tag &&
+               SLOT_TO_LOG_I(slots[i]) != INVALID_KV_I) {
                 int log_i = SLOT_TO_LOG_I(slots[i]);
-                PREFETCH(&ht_log[log_i]);
+                FPP_EXPENSIVE(&ht_log[log_i]);
                 
                 // Log entry also matches
                 if(ht_log[log_i].key == pkt_lo[batch_index]) {
@@ -31,7 +32,7 @@ void process_pkts_in_batch(LL *pkt_lo)
         }
         
         if(found == 0) {
-            fail_2 ++;
-        }
-    }
+            fail_2 ++; 
+        }   
+    }   
 }
