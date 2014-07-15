df2a831e767f9a126f94f9fcd235e7e668d80fbf
Anuj Kalia <akalia@cs.cmu.edu>
2014-07-15 15:46:23 -0600

    Fix label names and macros for MICA

--- antlr/actual/mica/nogoto.c
+++ antlr/actual/mica/nogoto.c
@@ -12,8 +12,6 @@
 // City hash of an unsigned number
 #define LL long long
 
-#define foreach(i, n) for(i = 0; i < n; i ++)
-
 // Compute an expensive hash using multiple applications of cityhash
 LL hash(LL key)
 {
@@ -68,7 +66,7 @@ void process_pkts_in_batch(LL *pkt_lo)
 		int key_tag = HASH_TO_TAG(key_hash);
 		int ht_bucket = HASH_TO_BUCKET(key_hash);
 
-		PREFETCH(&ht_index[ht_bucket]);
+		FPP_EXPENSIVE(&ht_index[ht_bucket]);
 		LL *slots = ht_index[ht_bucket].slots;
 
 		int i, found = 0;
@@ -79,7 +77,7 @@ void process_pkts_in_batch(LL *pkt_lo)
 			if(SLOT_TO_TAG(slots[i]) == key_tag && 
 				SLOT_TO_LOG_I(slots[i]) != INVALID_KV_I) {
 				int log_i = SLOT_TO_LOG_I(slots[i]);
-				PREFETCH(&ht_log[log_i]);
+				FPP_EXPENSIVE(&ht_log[log_i]);
 				
 				// Log entry also matches
 				if(ht_log[log_i].key == pkt_lo[batch_index]) {
