90ac48d2bd175f7e70d3984b766959d1446932ef
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-09-02 23:56:19 -0400

    Remove unused includes

--- l2fwd/main.h
+++ l2fwd/main.h
@@ -3,46 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <inttypes.h>
-#include <sys/types.h>
-#include <sys/queue.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <setjmp.h>
 #include <stdarg.h>
-#include <ctype.h>
-#include <errno.h>
-#include <getopt.h>
 #include <unistd.h>
 #include <assert.h>
 
 #include <rte_common.h>
-#include <rte_log.h>
-#include <rte_memory.h>
-#include <rte_memcpy.h>
-#include <rte_memzone.h>
-#include <rte_tailq.h>
-#include <rte_eal.h>
-#include <rte_per_lcore.h>
-#include <rte_launch.h>
-#include <rte_atomic.h>
 #include <rte_cycles.h>
 #include <rte_prefetch.h>
 #include <rte_lcore.h>
-#include <rte_per_lcore.h>
 #include <rte_branch_prediction.h>
-#include <rte_interrupts.h>
-#include <rte_pci.h>
-#include <rte_random.h>
-#include <rte_debug.h>
-#include <rte_ether.h>
 #include <rte_ip.h>
-#include <rte_udp.h>
 #include <rte_ethdev.h>
-#include <rte_ring.h>
 #include <rte_mempool.h>
 #include <rte_mbuf.h>
-#include <rte_byteorder.h>
 
 #include "sizes.h"
 
