e11a04959f2c608f38e625ff76ced04d45e667e3
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-09-14 18:18:55 -0400

    Some changes to .sh file parameters

--- gpu/mappedMemory/simple/run.sh
+++ gpu/mappedMemory/simple/run.sh
@@ -1,6 +1,6 @@
 #make
 
-for num_pkts in `seq 4 128 4096`; do
+for num_pkts in `seq 4 16 4096`; do
 	time=`taskset -c 0 ./mappedMemory $num_pkts | grep 5th | cut -d' ' -f 4`
 	echo "$num_pkts $time"
 	sleep .5
