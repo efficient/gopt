55750bef52fc41c3e978e14c459db0cd1af868d2
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-09-15 03:34:03 -0400

    Add rate-measurement to pointer chasing. Add options for running in L2 or L3 cache.

--- antlr/actual/pointer-chasing/param.h
+++ antlr/actual/pointer-chasing/param.h
@@ -2,9 +2,15 @@
 #define NUM_PKTS (1 * 1024 * 1024)
 
 #define LOG_SID 1
-#define LOG_CAP (128 * 1024 * 1024)		// Number of ints in the log
-#define LOG_CAP_ ((128 * 1024 * 1024) - 1)
 
-// Make the log fit inside L3
-//#define LOG_CAP (512 * 1024)		// Number of ints in the log
-//#define LOG_CAP_ ((512 * 1024) - 1)
+/** < 512 MB: DRAM */
+//#define LOG_CAP (128 * 1024 * 1024)		// Number of ints in the log
+//#define LOG_CAP_ (LOG_CAP - 1)
+
+/** < 16 MB: L3 cache */
+//#define LOG_CAP (4 * 1024 * 1024)			// Number of ints in the log
+//#define LOG_CAP_ (LOG_CAP - 1)
+
+/** < 256K: L2 cache */
+#define LOG_CAP (64 * 1024)					// Number of ints in the log
+#define LOG_CAP_ (LOG_CAP - 1)
