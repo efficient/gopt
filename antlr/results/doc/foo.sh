7485e7dcc0312ffd1d7136c16a900f6c04b1d5ef
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-09-17 19:41:34 -0400

    Remove ANTLR and GPU benchmarks from branch cuckoo - they should be in master

--- antlr/results/doc/foo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make clean
-make pdf
-open paper.pdf
--- antlr/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp ~/Documents/workspace/fastpp/src/* .
-cp -r ~/Documents/workspace/fastpp/test .
-
-# The `actuals` folder is only modified at xia-router2
