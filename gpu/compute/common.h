7ee2d0a1299919843179ca528c07122006129f95
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-09-25 01:20:59 -0400

    Increase number of packets for CPU-vs-GPU compute

--- gpu/compute/common.h
+++ gpu/compute/common.h
@@ -1,8 +1,8 @@
 #define INCLUDE_COPY_TIME 1
 
-#define ITERS 10000			/** < Number of measurements to average on */
+#define ITERS 100			/** < Number of measurements to average on */
 
-#define MAX_PKTS (1024 * 32)
+#define MAX_PKTS (32 * 1024 * 1024)
 
 #define CPE(val, msg, err_code) \
 	if(val) { fprintf(stderr, msg); fprintf(stderr, " Error %d \n", err_code); \
