430fd7ba5813c3a77a96aa65050a9b431165ec4f
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-11-27 05:02:00 -0500

    Add cpus options to antlr/ht-cuckoo

--- antlr/actual/ht-cuckoo/handopt.sh
+++ antlr/actual/ht-cuckoo/handopt.sh
@@ -9,5 +9,12 @@ blue ""
 blue "Running handopt"
 shm-rm.sh 1>/dev/null 2>/dev/null
 
+# xia-r2
+cpus=0,2,4,6,8,10,12,14
+
+# Apt c6220
+# cpus=0-7
+
 num_threads=8
-sudo numactl --physcpubind=0-7 --membind=0 ./handopt $num_threads
+
+sudo numactl --physcpubind=$cpus --membind=0 ./handopt $num_threads
