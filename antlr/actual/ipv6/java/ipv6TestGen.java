8cc284ddf0c17913535ec467d12dda03655ec2d7
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-12-17 19:37:03 -0500

    Ensure that a probe address matches a unique longest prefix -- avoid ambiguous behavior when comparing with DPDK lpm

--- antlr/actual/ipv6/java/ipv6TestGen.java
+++ antlr/actual/ipv6/java/ipv6TestGen.java
@@ -5,7 +5,7 @@ import java.util.*;
 /*< Class to generate prefixes and probe IPv6 addresses for testing */
 public class ipv6TestGen {
 	static int num_prefixes = 32;
-	static int num_ips = 8;
+	static int num_ips = 32;
 	static int max_prefix_len = 48;
 
 	public static int rand() {
@@ -25,19 +25,16 @@ public class ipv6TestGen {
 		return ret.substring(0, len);
 	} 
 
+	/*< An address matches a prefix (length = len) if its bit representation
+	 *  up to len bits is same as the prefix */
 	public static boolean match(IPv6Prefix p, IPv6Address a) {
-		String pBytes = Arrays.toString(p.bytes);
-		String aBytes = Arrays.toString(a.bytes);
-
 
 		int len = p.len;
+
 		String pBits = getBits(p.bytes, len);
 		String aBits = getBits(a.bytes, len);
 		boolean ret = pBits.equals(aBits);
 
-		System.out.printf("Matching prefix %s, %d with address %s --> %B\n",
-			pBytes, p.len, aBytes, ret);
-	
 		return ret;
 	}
 
@@ -47,32 +44,34 @@ public class ipv6TestGen {
 		IPv6Address addresses[] = new IPv6Address[num_ips];
 
 		/*< Generate the prefixes */
+		System.out.println("Generating prefixes");
+
 		for(int i = 0; i < num_prefixes; i ++) {
 			prefixes[i] = new IPv6Prefix();
 
 			/*< Length of this prefix in bits */
 			prefixes[i].len = (rand() % max_prefix_len) + 1;
-			System.out.print(prefixes[i].len + " ");
+			System.out.print(prefixes[i].len + "  ");
 
 			for(int j = 0; j < 16; j ++) {
 				prefixes[i].bytes[j] = (byte) (rand() % 4);
 				System.out.print(prefixes[i].bytes[j] + " ");
 			}
 
-			prefixes[i].dst_port = rand() % 8;
-			System.out.println(prefixes[i].dst_port);
+			prefixes[i].dst_port = rand() % 256;
+			System.out.println(" " + prefixes[i].dst_port);
 		}
 
 		/*< Generarate the probe IPs */
+		System.out.println("Generating IPs");
 		for(int i = 0; i < num_ips; i ++) {
 			addresses[i] = new IPv6Address();
 
 			for(int j = 0; j < 16; j ++) {
 				addresses[i].bytes[j] = (byte) (rand() % 4);
-				System.out.print(addresses[i].bytes[j] + " ");
 			}
 
-			int dst_port = -1, longest_match = -1;
+			int dst_port = -1, lpm_len = -1;
 
 			/*< Find the longest match among all prefixes */
 			for(int j = 0; j < num_prefixes; j ++) {
@@ -81,14 +80,35 @@ public class ipv6TestGen {
 				if(match(prefixes[j], addresses[i])) {
 					int match_len = prefixes[j].len;
 
-					if(match_len > longest_match) {
-						longest_match = match_len;
+					if(match_len > lpm_len) {
+						lpm_len = match_len;
 						dst_port = prefixes[j].dst_port;
 					}
 				}
 			}
 
-			System.out.println(dst_port);
+			/*< If > 1 prefixes are LPM matches for this address, try a
+			  * different address */
+			int lpm_prefixes = 0;
+			for(int j = 0; j < num_prefixes; j ++) {
+				if(match(prefixes[j], addresses[i])) {
+					if(prefixes[j].len == lpm_len) {
+						lpm_prefixes ++;
+					}
+				}
+			}
+
+			if(lpm_prefixes > 1) {
+				i --;
+				continue;
+			}
+			
+
+			for(int j = 0; j < 16; j ++) {
+				System.out.print(addresses[i].bytes[j] + " ");
+			}
+
+			System.out.println(" " + dst_port);
 		}
 		
 	}
