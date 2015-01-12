6802da3616f527b6953ccde208d74980918a4341
Anuj Kalia <anujkaliaiitd@gmail.com>
2015-01-12 02:12:21 -0500

    Default settings for pointer chasing benchmark

--- antlr/actual/pointer-chasing/param.h
+++ antlr/actual/pointer-chasing/param.h
@@ -1,11 +1,11 @@
-#define DEPTH 1500
+#define DEPTH 100
 #define NUM_PKTS (64 * 1024)
 
 #define LOG_SID 1
 
 /**< 512 MB: DRAM */
-//#define LOG_CAP (128 * 1024 * 1024)		// Number of ints in the log
-//#define LOG_CAP_ (LOG_CAP - 1)
+#define LOG_CAP (128 * 1024 * 1024)		// Number of ints in the log
+#define LOG_CAP_ (LOG_CAP - 1)
 
 /**< 16 MB: L3 cache */
 //#define LOG_CAP (4 * 1024 * 1024)			// Number of ints in the log
@@ -16,5 +16,5 @@
 //#define LOG_CAP_ (LOG_CAP - 1)
 
 /**< 32K: L1 cache */
-#define LOG_CAP (2 * 1024)					// Number of ints in the log
-#define LOG_CAP_ (LOG_CAP - 1)
+//#define LOG_CAP (2 * 1024)					// Number of ints in the log
+//#define LOG_CAP_ (LOG_CAP - 1)
