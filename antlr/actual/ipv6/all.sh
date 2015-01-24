b1d6f7ccb8dafd29db5f46fd7c3c220aa4da2830
Anuj Kalia <anujkaliaiitd@gmail.com>
2015-01-24 16:57:35 -0500

    Add local IPv6 results

--- antlr/actual/ipv6/all.sh
+++ antlr/actual/ipv6/all.sh
@@ -1,3 +1,11 @@
-./nogoto.sh
-./goto.sh
-./handopt.sh
+rm -rf results
+mkdir results
+
+echo "Running nogoto"
+./nogoto.sh > results/nogoto_out
+
+echo "Running goto"
+./goto.sh > results/goto_out
+
+echo "Running handopt"
+./handopt.sh > results/handopt_out
