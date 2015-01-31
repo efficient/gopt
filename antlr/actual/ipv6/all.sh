e70a40af5e0ff2596fdfc65699cd0ce0eb09e7cf
Anuj Kalia <anujkaliaiitd@gmail.com>
2015-01-31 00:02:05 -0500

    Don't redirect results

--- antlr/actual/ipv6/all.sh
+++ antlr/actual/ipv6/all.sh
@@ -2,10 +2,10 @@ rm -rf results
 mkdir results
 
 echo "Running nogoto"
-./nogoto.sh > results/nogoto_out
+./nogoto.sh
 
 echo "Running goto"
-./goto.sh > results/goto_out
+./goto.sh
 
 echo "Running handopt"
-./handopt.sh > results/handopt_out
+./handopt.sh
