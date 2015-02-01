5d4c3e2abd08dee593d5baa74d987fb4c03393bd
Anuj Kalia <anujkaliaiitd@gmail.com>
2015-02-01 18:23:45 -0500

    Code style fixes

--- l2fwd/server.c
+++ l2fwd/server.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-// Only immutable information should be global
-// xia-router2 xge0,1,2,3,4,5,6,7
+/**< Only immutable information should be global
+  *  xia-router2 xge0,1,2,3,4,5,6,7 */
 LL src_mac_arr[8] = {0x6c10bb211b00, 0x6d10bb211b00, 0x64d2bd211b00, 0x65d2bd211b00,
 					 0xc8a610ca0568, 0xc9a610ca0568, 0xa2a610ca0568, 0xa3a610ca0568};
 
-// xia-router0 xge0,1    xia-router1 xge0,1    xia-router0 xge2,3    xia-router1 xge2,3
+/**< xia-router0 xge0,1    xia-router1 xge0,1
+  *  xia-router0 xge2,3    xia-router1 xge2,3 */
 LL dst_mac_arr[8] = {0x36d3bd211b00, 0x37d3bd211b00, 0x44d7a3211b00, 0x45d7a3211b00,
 					 0xa8d6a3211b00, 0xa9d6a3211b00, 0x0ad7a3211b00, 0x0bd7a3211b00};
 
@@ -29,13 +30,13 @@ void send_packet(struct rte_mbuf *pkt, int port_id,
 	lp_info[port_id].mbufs[tot_buffered] = pkt;
 	tot_buffered ++;
 
-	// TX when a sufficient number of packets are buffered
+	/**< TX when a sufficient number of packets are buffered */
 	if(unlikely(tot_buffered == MAX_SRV_BURST)) {
 		int queue_id = lp_info[port_id].queue_id;
 		int nb_tx_new = rte_eth_tx_burst(port_id, queue_id, 
 			lp_info[port_id].mbufs, MAX_SRV_BURST);
 
-		// Free unsent packets
+		/**< Free unsent packets */
 		for(i = nb_tx_new; i < MAX_SRV_BURST; i ++) {
 			rte_pktmbuf_free(lp_info[port_id].mbufs[i]);
 		}
@@ -50,12 +51,11 @@ void send_packet(struct rte_mbuf *pkt, int port_id,
 	}
 }
 
-int batch_index = 0;
-
 void process_batch_nogoto(struct rte_mbuf **pkts, int nb_pkts, int port_id,
 	const struct rte_lpm *lpm, 
 	struct lcore_port_info *lp_info)
 {
+	int batch_index = 0;
 	foreach(batch_index, nb_pkts) {
 
 		/**< Boilerplate for TX pkt */
@@ -209,7 +209,7 @@ void run_server(struct rte_lpm *lpm)
 	int num_active_ports = bitcount(XIA_R2_PORT_MASK);
 	int *port_arr = get_active_bits(XIA_R2_PORT_MASK);
 	
-	// Initialize the per-port info for this lcore
+	/**< Initialize the per-port info for this lcore */
 	struct lcore_port_info lp_info[RTE_MAX_ETHPORTS];
 	memset(lp_info, 0, RTE_MAX_ETHPORTS * sizeof(struct lcore_port_info));
 	for(i = 0; i < RTE_MAX_ETHPORTS; i ++) {
@@ -262,22 +262,20 @@ void run_server(struct rte_lpm *lpm)
 			double seconds = nanoseconds / GHZ_CPS;
 			tput_tsc[0] = tput_tsc[1];
 
-			red_printf("Lcore %d, total: %f, tbl_24: %f%\n", lcore_id, 
-				lp_info[0].nb_tx_all_ports / seconds,
-				(lp_info[0].nb_tbl_24 * 100.0) / lp_info[0].nb_tx_all_ports);
+			red_printf("Lcore %d, total: %f\n", lcore_id, 
+				lp_info[0].nb_tx_all_ports / seconds);
 
+			/**< Reset all-port stats in case port 0 is disabled */
+			lp_info[0].nb_tx_all_ports = 0;
 			for(i = 0; i < RTE_MAX_ETHPORTS; i++) {
 				if(ISSET(XIA_R2_PORT_MASK, i)) {
 					printf("\tLcore: %d, port: %d: %f\n", lcore_id, i, 
 						lp_info[i].nb_tx / seconds);
 				}
 
-				// Do not reset the nb_buf counter
+				/**< Do not reset the nb_buf counter */
 				lp_info[i].nb_tx = 0;
 				lp_info[i].nb_rx = 0;
-	
-				lp_info[i].nb_tbl_24 = 0;
-				lp_info[i].nb_tx_all_ports = 0;
 			}
 
 			printf("\tLcore %d, Average TX burst size: %lld\n", lcore_id, 
