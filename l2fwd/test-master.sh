6dda5e41415fb6d6917ff25d8209e51aab1ae5c0
Anuj Kalia <anujkaliaiitd@gmail.com>
2015-02-22 02:23:04 -0500

    Fix kernel test script

--- l2fwd/test-master.sh
+++ l2fwd/test-master.sh
@@ -12,5 +12,10 @@ nvcc -O3 -o master -gencode arch=compute_35,code=compute_35 util.c ipv6.c rte_lp
 blue "Killing existing GPU-master processes"
 sudo killall master
 
+blue "Removing DPDK's hugepages and shm key 1, 2, 3"
+sudo rm -rf /mnt/huge/*
+sudo ipcrm -M 1			# WM_QUEUE_KEY
+sudo ipcrm -M 2			# RTE_LPM6_SHM_KEY
+
 worker_lcore_mask="0x1"
 sudo taskset -c 14 ./master -c $worker_lcore_mask
